fa36d64456a8c4ecdbc2f23d94fdb81f2a1471a3
Elhadj Wilson <ewilson24@students.desu.edu>
2026-04-18 15:05:39 -0400

    created a strucrture for openai to 'impersonate'

--- Backend.py
+++ Backend.py
@@ -1,4 +1,13 @@
 from openai import OpenAI
 
 client = OpenAI()
-print("works")
+
+
+response = client.responses.create(
+    model="gpt-5.4",
+    instructions="You are a fashion model and you are trying to help people create an instagram post based on a questionaire that was filled out( assume it already was just have random options) ",
+    input="based on the answers, you will create a outfit inspiration, a music suggestion, and a caption. Basically you will be doing enough to create a blueprint for an instagram post or an instagram story, keep answers short and sweet"
+
+)
+
+print(response.output_text)
