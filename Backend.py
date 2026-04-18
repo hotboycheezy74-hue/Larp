f821d8fe9e3c861be97f5da062ae13e124cd05d4
Elhadj Wilson <ewilson24@students.desu.edu>
2026-04-18 16:34:02 -0400

    created additional files for organization

--- Backend.py
+++ Backend.py
@@ -1,13 +1,22 @@
 from openai import OpenAI
+from InfoModule import appearanceSelection, musicSelection, vibeSelection
 
-client = OpenAI()
+def main():
+
+    client = OpenAI()
 
 
-response = client.responses.create(
-    model="gpt-5.4",
-    instructions="You are a fashion model and you are trying to help people create an instagram post based on a questionaire that was filled out( assume it already was just have random options) ",
-    input="based on the answers, you will create a outfit inspiration, a music suggestion, and a caption. Basically you will be doing enough to create a blueprint for an instagram post or an instagram story, keep answers short and sweet"
+    response = client.responses.create(
+        model="gpt-5.4",
+        instructions="You are a fashion model and you are trying to help people create an instagram post based on a questionaire that was filled out( assume it already was just have random options) ",
+        input="based on the answers, you will create a outfit inspiration, a music suggestion, and a caption. Basically you will be doing enough to create a blueprint for an instagram post or an instagram story, keep answers short and sweet"
+    )
 
-)
 
-print(response.output_text)
+
+    print(response.output_text)
+
+
+#calls the main function
+if __name__ == "__main__":
+    main()
