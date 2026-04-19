fde504a7a8c5b9684894596d56c4fd6dbc217a29
Ausine Sieh <asiehh47@gmail.com>
2026-04-19 01:26:16 -0400

    a lot

--- Backend.py
+++ Backend.py
@@ -1,9 +1,9 @@
-from InfoModule import appearanceSelection, musicSelection, vibeSelection
 import openai_Module
 import spotify_module
 
 # --// Table we'll use to fill in from user input & AI output
 UserInputtedData = {
+    "Gender": "",
     "Music": {
         "Genre": "",
         "Energy": "",
@@ -25,18 +25,15 @@ TestInputData = { # --// For use case of a ready to go IG photo
     "Vibe": "dreamy",
     "Music": {
         "Genre": "rap",
-        "Energy": "laid_back",
-        "Era": "2000s",
+        "Energy": "aggressive",
+        "Era": "currently_trending",
         "Artist": "Lazer Dim 700",
     },
-
 }
-
-# "Tell me what is in this image, and if you think it is someone that exists in this world, tell me who it is (it could be a rapper)"
 
 #calls the main function
 if __name__ == "__main__":
-    spotify_module.search_artist_catalogue(TestInputData)
+    print(spotify_module.search_general_song(TestInputData))
     #spotify_module.search_general_song(TestInputData)
     #openai_Module.get_outfit_style()
     #main()
--- spotify_module.py
+++ spotify_module.py
@@ -1,5 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import openai_Module
 
 CLIENT_ID = "your_client_id"
 CLIENT_SECRET = "your_client_secret"
@@ -13,10 +14,16 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 
 def search_artist_catalogue(inputdata):
     results = sp.search(q=inputdata['Music']['Artist'], type=["track"], limit=10)
-    for track in results["tracks"]["items"]:
-        print(track["name"], "-", track["artists"][0]["name"])
+    #for track in results["tracks"]["items"]:
+        #print(track["name"], "-", track["artists"][0]["name"])
+
+    promptstring = f"Here is a list of 10 songs from a specific artist, give three songs from this list that most accurately fits these key words: {inputdata['Music']['Energy']}, {inputdata['Photo Vibe'] or inputdata['Vibe']}, {results}, and only output the list, no other words"
+    return openai_Module.ai_request(promptstring, False).output_text
 
 def search_general_song(inputdata):
-    results = sp.search(q=(inputdata['Music']['Genre'] + inputdata['Music']['Era'] + inputdata['Music']['Era'] + (inputdata['Photo Vibe'] or inputdata['Music']['Energy'])), type=["track"], limit=10)
-    for track in results["tracks"]["items"]:
-        print(track["name"], "-", track["artists"][0]["name"])
+    results = sp.search(q=(inputdata['Music']['Genre'] + inputdata['Music']['Era']), type=["track"], limit=10)
+    #for track in results["tracks"]["items"]:
+        #print(track["name"], "-", track["artists"][0]["name"])
+
+    promptstring = f"Here is a list of 10 songs, give three songs from this list that most accurately fits these key words: {inputdata['Music']['Energy']}, {inputdata['Photo Vibe'] or inputdata['Vibe']}, {results}, and only output the list, no other words"
+    return openai_Module.ai_request(promptstring, False).output_text
