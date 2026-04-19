31dc3acfa532079cf470dcef052f136a0a8db834
Ausine Sieh <asiehh47@gmail.com>
2026-04-18 20:41:37 -0400

    Edited modules, cleaned up main script, etc

--- Backend.py
+++ Backend.py
@@ -1,20 +1,37 @@
-import openai_client
 from InfoModule import appearanceSelection, musicSelection, vibeSelection
+import openai_Module
 
-def main():
-    """
-    response = response.responses.create(
-        model="gpt-5.4",
-        instructions="You are a fashion model and you are trying to help people create an instagram post based on a questionaire that was filled out( assume it already was just have random options) ",
-        input="based on the answers, you will create a outfit inspiration, a music suggestion, and a caption. Basically you will be doing enough to create a blueprint for an instagram post or an instagram story, keep answers short and sweet"
-    )
-    print(response.output_text)
-    """
+# --// Table we'll use to fill in from user input & AI output
+UserInputtedData = {
+    "Music": {
+        "Genre": "",
+        "Energy": "",
+        "Era": "",
+        "Artist": "",
+    },
+    "Vibe": "",
+    "Appearance": {
+        "Style" : "",
+        "ColorPalette" : "",
+        "Accessories" : "",
+        "Presentation" : "",
+    }
+}
+
+# --// Test inputs before working on front end
+TestInputData = { # --// For use case of a ready to go IG photo
+    "Music": {
+        "Genre": "rap",
+        "Energy": "laid_back",
+        "Era": "timeless",
+        "Artist": "Playboi Carti",
+    },
+    "Vibe": "dreamy",
+}
 
 # "Tell me what is in this image, and if you think it is someone that exists in this world, tell me who it is (it could be a rapper)"
-# noinspection PyTypeChecker
 
-
+#calls the main function
 if __name__ == "__main__":
-    openai_client.testLazerDim()
-        #main()
+    openai_Module.backendtest(TestInputData)
+    #main()
