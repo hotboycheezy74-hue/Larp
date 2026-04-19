672d1974f33047f0244867f5522b58b9a43ac62f
austinesieh <asiehh47@gmail.com>
2026-04-19 11:21:36 -0400

    Update pexels_module.py

--- backend/pexels_module.py
+++ backend/pexels_module.py
@@ -1,5 +1,3 @@
-from xml.etree.ElementTree import tostring
-
 import requests
 
 API_KEY = "Idf81fua0DBkxBjXozcUhfY4dLjFTaXAOH8VTpM4MQdUUBkuuotxXiNx"
@@ -10,15 +8,13 @@ headers = {
 
 url = "https://api.pexels.com/v1/search?query=streetwear outfit&per_page=5"
 
-def search_images(query, limit=5):
-    url = f"https://api.pexels.com/v1/search?query={query}&per_page={limit}"
+def search_images(UserInfo, limit=3):
+    search_string = f"{UserInfo["Photo Appearance"]["Style"]} {UserInfo["Photo Appearance"]["Presentation"]} {UserInfo["Photo Vibe"]} Outfit"
+    url = f"https://api.pexels.com/v1/search?query={search_string}&per_page={limit}"
     r = requests.get(url, headers=headers)
-    return r.json()
+    image_info = r.json()
 
-imageurls = []
-image_info = search_images("male confident streetwear outfit")
-for photo in image_info["photos"]:
-    imageurls.append(photo["src"]["original"])
-
-
-print(imageurls)
+    imageurls = []
+    for photo in image_info["photos"]:
+        imageurls.append(photo["src"]["original"])
+    return imageurls
